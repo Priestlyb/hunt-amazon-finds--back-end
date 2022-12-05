992e92af87389211b1846754c8366ffa0cbaabb7
Priestly Patrick bassey <pbassey30@gmail.com>
2022-12-05 09:51:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require ('mongoose')
+const router = require("./routes/book-routes")
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.use("/books", router) // localhost:5000/books
+
+app.use(express.static(path.join(__dirname, "/fron-end/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/front-end/build', 'index.html'));
+});
+
+
+/* Click connect at Database Deployments page */
+mongoose.connect(
+    "mongodb+srv://mernApp:p%40ssw0rd%279%27%21@mernbookstore.iwzp06a.mongodb.net/?retryWrites=true&w=majority"
+        )
+        .then(()=>console.log("Connected to Datebase"))
+        .then(() => {
+            ;app.listen(process.env.PORT || 3000)
+        }).catch((err) =>console.log(err));
