84ff721bd8a8b5d5ce757d2723fa92a1cada7aa3
Priestly Patrick bassey <pbassey30@gmail.com>
2025-06-03 18:19:34 +0100

    Made the mongo url env property

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const mongoose = require ('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-const router = require("./routes/product-routes")
+const router = require("./routes/product-routes");
 const app = express();
-const cors = require('cors');
-
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://huntamazonfinds.vercel.app',  // https://huntamazonfinds.vercel.app , http://localhost:3000
-  credentials: true, 
-}));
+app.use(
+  cors({
+    origin: "https://huntamazonfinds.vercel.app", // https://huntamazonfinds.vercel.app , http://localhost:3000
+    credentials: true,
+  })
+);
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
-app.use("/products", router) // localhost:5000/books
-
+app.use("/products", router); // localhost:5000/books
 
 /* Click connect at Database Deployments page */
-mongoose.connect(
-    "mongodb+srv://mernApp:p%40ssw0rd%279%27%21@mernbookstore.iwzp06a.mongodb.net/?retryWrites=true&w=majority"
-        )
-        .then(()=>console.log("Connected to Datebase"))
-        .then(() => {
-            app.listen(process.env.PORT || 5001);
-        }).catch((err) =>console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(process.env.PORT || 5001, () => {
+      console.log(`Server running on port ${process.env.PORT || 5001}`);
+    });
+  })
+  .catch((err) => console.log(err));
